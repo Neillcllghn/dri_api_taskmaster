b178171ba6e45aeb556eb1225f0e2bbeaa5166e8
Neillcllghn <>
2023-08-11 18:32:44 +0000

    updating heroku url for deployment

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -64,7 +64,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = ['localhost',
-                 'https://taskmaster-django-rest-5e7a6a0f075a.herokuapp.com/']
+                 'taskmaster-django-rest-5e7a6a0f075a.herokuapp.com']
 
 # ALLOWED_HOSTS = ['8000-neillcllghn-dri-api-task-seg98p89jd.us2.codeanyapp.com']
 
@@ -108,12 +108,13 @@ MIDDLEWARE = [
 ]
 
 if 'CLIENT_ORIGIN' in os.environ:
-    extracted_url = re.match(
-        r'^([^.]+)', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
-
+    CORS_ALLOWED_ORIGINS = [
+        os.environ.get('CLIENT_ORIGIN')
+        ]
+else:
     CORS_ALLOWED_ORIGIN_REGEXES = [
-        rf"{extracted_url}.(eu|us)\d+\.codeanyapp\.com$",
-    ]
+        r"^https://.*\.gitpod\.io$",
+        ]
 
 CORS_ALLOW_CREDENTIALS = True
 
