fa4d0dae15918964b502d3d5c22f2a047b9c5949
Neillcllghn <>
2023-08-11 17:36:43 +0000

    Preparing the API for deployment by adding pagination and date formating

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -8,7 +8,6 @@ class TaskSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     due_date = serializers.DateField(required=False)
-    created_at = serializers.SerializerMethodField()
     updated_at = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
@@ -22,9 +21,6 @@ class TaskSerializer(serializers.ModelSerializer):
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
-
-    def get_created_at(self, obj):
-        return naturaltime(obj.created_at)
 
     def get_updated_at(self, obj):
         return naturaltime(obj.updated_at)
