4b9f5c7198063a50604f9be4eff45b3e9afd359d
Neillcllghn <>
2023-08-25 17:48:32 +0000

    updating settings.py for redeployment of API to Heroku

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -66,12 +66,12 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-# ALLOWED_HOSTS = [
-#     os.environ.get('ALLOWED_HOST'),
-#     'localhost',
-# ]
+ALLOWED_HOSTS = [
+    os.environ.get('ALLOWED_HOST'),
+    'localhost',
+]
 
-ALLOWED_HOSTS = ['8000-neillcllghn-dri-api-task-seg98p89jd.us2.codeanyapp.com']
+# ALLOWED_HOSTS = ['8000-neillcllghn-dri-api-task-seg98p89jd.us2.codeanyapp.com']
 
 # Application definition
 
