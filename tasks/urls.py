71cf7714187f59cd3dcd698d1da740b4cc350727
Neillcllghn <>
2023-09-20 16:28:39 +0000

    Add API endpoints for task counts

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,4 +4,8 @@ from tasks import views
 urlpatterns = [
     path('tasks/', views.TaskList.as_view()),
     path('tasks/<int:pk>/', views.TaskDetail.as_view()),
+    path('incomplete-task-count/', views.IncompleteTaskCountView.as_view(),
+         name='incomplete-task-count'),
+    path('urgent-task-count/', views.UrgentTaskCountView.as_view(),
+         name='urgent-task-count'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -3,6 +3,10 @@ from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Task
 from .serializers import TaskSerializer, TaskDetailSerializer
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 
 class TaskList(generics.ListCreateAPIView):
@@ -31,3 +35,25 @@ class TaskDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [IsOwnerOrReadOnly]
     serializer_class = TaskDetailSerializer
     queryset = Task.objects.all()
+
+
+class IncompleteTaskCountView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        incomplete_count = Task.objects.filter(
+            owner=user, completed=False).count()
+        return Response({'incomplete_task_count': incomplete_count},
+                        status=status.HTTP_200_OK)
+
+
+class UrgentTaskCountView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        urgent_count = Task.objects.filter(
+            owner=user, completed=False, is_urgent=True).count()
+        return Response({'urgent_task_count': urgent_count},
+                        status=status.HTTP_200_OK)
